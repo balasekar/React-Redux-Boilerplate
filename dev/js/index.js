126f2abd0a15f7bf85d409e4d596f20c6bbba4a0
balasekar <baalasekar@gmail.com>
2017-08-24 12:47:21 +0100

    Store and Reducers Complete

--- dev/js/index.js
+++ dev/js/index.js
@@ -1,23 +1,9 @@
 import 'babel-polyfill';
 import React from 'react';
-import ReactDOM from "react-dom";
-import {Provider} from 'react-redux';
-import {createStore, applyMiddleware} from 'redux';
-import thunk from 'redux-thunk';
-import promise from 'redux-promise';
-import createLogger from 'redux-logger';
+import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
 import allReducers from './reducers';
-import App from './components/App';
 
-const logger = createLogger();
-const store = createStore(
-    allReducers,
-    applyMiddleware(thunk, promise, logger)
-);
 
-ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    document.getElementById('root')
-);
+const store = createStore(allReducers);
+ReactDOM.render( <Provider store={store}> <App /> </Provider>, document.getElementById('root'));
--- dev/js/reducers/reducer-users.js
+++ dev/js/reducers/reducer-users.js
@@ -1,33 +1,28 @@
-/*
- * The users reducer will always return an array of users no matter what
- * You need to return something, so if there are no users then just return an empty array
- * */
-
-export default function () {
+export default function() {
     return [
         {
             id: 1,
-            first: "Bucky",
-            last: "Roberts",
-            age: 71,
-            description: "Bucky is a React developer and YouTuber",
-            thumbnail: "http://i.imgur.com/7yUvePI.jpg"
+            first: "Bala",
+            last: "Sekar",
+            age: "28",
+            description: "Bala is a React developer",
+            thumbnail: ""
         },
         {
             id: 2,
-            first: "Joby",
-            last: "Wasilenko",
-            age: 27,
-            description: "Joby loves the Packers, cheese, and turtles.",
-            thumbnail: "http://i.imgur.com/52xRlm8.png"
+            first: "Kala",
+            last: "Sekar",
+            age: "48",
+            description: "Kala is a Chef",
+            thumbnail: ""
         },
         {
             id: 3,
-            first: "Madison",
-            last: "Williams",
-            age: 24,
-            description: "Madi likes her dog but it is really annoying.",
-            thumbnail: "http://i.imgur.com/4EMtxHB.png"
+            first: "Sekar",
+            last: "Bala",
+            age: "60",
+            description: "Sekar is a Chemical Engineer",
+            thumbnail: ""
         }
-    ]
+    ];
 }
