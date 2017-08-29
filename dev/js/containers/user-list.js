694c849b0e6136e5f7ec32d2ff701a2ad3364f05
balasekar <baalasekar@gmail.com>
2017-08-29 10:54:29 +0100

    Alignment fix

--- dev/js/containers/user-list.js
+++ dev/js/containers/user-list.js
@@ -7,10 +7,7 @@ class UserList extends Component {
     createListItems(){
         return this.props.users.map((user) => {
            return (
-               <li
-                   key={user.id}
-                   onClick={() =>this.props.selectUser(user)}
-               >{user.first} {user.last} </li>
+               <li key={user.id} onClick={() =>this.props.selectUser(user)}>{user.first} {user.last} </li>
            );
         });
     }
